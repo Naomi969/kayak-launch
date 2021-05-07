0fb258f77be48390044c8b80f61d1d46aa8919cc
William Crews <crewswh@gmail.com>
2021-05-06 21:24:05 -0400

    pretty page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 /// Darkmode button and Dingo picture background
 
-var DARTHMODE = element.body.button
-
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
