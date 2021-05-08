155cc3844a75625dc40f50a2f3978ae7b3e71f93
William Crews <crewswh@gmail.com>
2021-05-08 11:08:11 -0400

    button is online

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
-var john = document.querySelector('john');
+var john = document.querySelector('#john');
+var order = document.querySelector('#order');
 
 
+
+
+john.addEventListener('click', updateorder);
+
+function updateorder (event) {
+  if (john.value === 'click') {
+    john.value = 'click';
+    console.log('john');
+  } else {
+    john.value = 'Start machine';
+    order.textContent = 'The machine is stopped.';
+  }
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/button - machine is stopped indeed.
 
