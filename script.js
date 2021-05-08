f3ddab007195b92904f084bc113bec5bc8ec7389
William Crews <crewswh@gmail.com>
2021-05-08 13:25:41 -0400

    yay

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 
-// //Current Weather function//
-// var date = moment().format("L");
-// var key = '957c1d427eb08dc32b2d83caeea47227'
+//Current Weather function//
+var date = moment().format("L");
+var key = '957c1d427eb08dc32b2d83caeea47227'
 
-// function curWeather(locale) {
-//     var qUrl = `https://api.openweathermap.org/data/2.5/weather?q=${locale}&units=imperial&appid=957c1d427eb08dc32b2d83caeea47227`;
+function curWeather(locale) {
+    var qUrl = `https://api.openweathermap.org/data/2.5/weather?q=${locale}&units=imperial&appid=957c1d427eb08dc32b2d83caeea47227`;
      
-//     fetch(qUrl)
-//     .then(function(response) {
-//         return response.json();
-//     })
-//     .then(function (data){
-//         var icon = data.weather[0].icon;
-//         var iUrl = `https://openweathermap.org/img/wn/${icon}.png`;
-//         var cityData = $(`
-//                     <h3> ${data.name}  ${date} <img src="${iUrl}" alt="${data.weather[0].description}"</h3>
-//                     <p> Current Temp: ${data.main.temp}\u00B0 F </p>
-//                     <p> Wind Speed:  ${data.wind.speed} mph </p>
-//                     <p> Humidity: ${data.main.humidity} \% </p>`);
+    fetch(qUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function (data){
+        var icon = data.weather[0].icon;
+        var iUrl = `https://openweathermap.org/img/wn/${icon}.png`;
+        var cityData = $(`
+                    <h3> ${data.name}  ${date} <img src="${iUrl}" alt="${data.weather[0].description}"</h3>
+                    <p> Current Temp: ${data.main.temp}\u00B0 F </p>
+                    <p> Wind Speed:  ${data.wind.speed} mph </p>
+                    <p> Humidity: ${data.main.humidity} \% </p>`);
                     
        
-//         $("#todayWeather").append(cityData);
-//     })
+        $("#todayWeather").append(cityData);
+    })
     
 
-// };       
+};       
 
 
 // WM's nonsense:
@@ -47,8 +47,6 @@ function updateorder (event) {
 
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/button - machine is stopped indeed.
 
-
-};       
 
 //Add primary search (city) from page1 to local storage
 
