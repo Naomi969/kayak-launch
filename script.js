b7ac9d7605a04cba8ceadc2e25f5f4d7f44a6dd2
Rob Brunett <rb3@Robs-Air.attlocal.net>
2021-05-10 20:32:41 -0400

    linked pages with button click

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-
-//Current Weather function//
-var date = moment().format("L");
-var key = '957c1d427eb08dc32b2d83caeea47227'
-
-function curWeather(locale) {
-    var qUrl = `https://api.openweathermap.org/data/2.5/weather?q=${locale}&units=imperial&appid=957c1d427eb08dc32b2d83caeea47227`;
-     
-    fetch(qUrl)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function (data){
-        var icon = data.weather[0].icon;
-        var iUrl = `https://openweathermap.org/img/wn/${icon}.png`;
-        var cityData = $(`
-                    <h3> ${data.name}  ${date} <img src="${iUrl}" alt="${data.weather[0].description}"</h3>
-                    <p> Current Temp: ${data.main.temp}\u00B0 F </p>
-                    <p> Wind Speed:  ${data.wind.speed} mph </p>
-                    <p> Humidity: ${data.main.humidity} \% </p>`);
-                    
-       
-        $("#todayWeather").append(cityData);
-    })
-
 // //Current Weather function//
  var date = moment().format("L");
  var key = '957c1d427eb08dc32b2d83caeea47227'
@@ -42,20 +17,14 @@ function curWeather(locale) {
                      <p> Current Temp: ${data.main.temp}\u00B0 F </p>
                      <p> Wind Speed:  ${data.wind.speed} mph </p>
                      <p> Humidity: ${data.main.humidity} \% </p>`);
-                    
-       
-
-//        $("#weather").append(cityData);
-//    })
-
-    
-
+        $("#weather").append(cityData);
+   })
 };       
 
 
 // WM's nonsense:
 
-var john = document.querySelector('#john');
+/*var john = document.querySelector('#john');
 var order = document.querySelector('#order');
 var userInput = document.querySelector('#userInput')
 var mapDisplay = $('#map');
@@ -70,18 +39,20 @@ function updateorder (event) {
     john.value = 'Start machine';
     order.textContent = 'userValue';
   }
-}
+}*/
 
 
 //Add primary search (city) from page1 to local storage
 
 var storeCity = [];
 
-$("#john").on("click", function(event) {
+$("#john", "#userChoiceBtn").on("click", function(event) {
     var userCity = $("#userInput").val();
     storeCity.push(userCity);
     localStorage.setItem("cities", JSON.stringify(storeCity))
-});
+    displayCityBtn();
+    curWeather();
+
 
 //load saved cities from local storage
 function loadCity() {
@@ -89,15 +60,15 @@ function loadCity() {
     if (loadCity) {
         city = JSON.parse(savedCity);
         city.reverse();
-        displayCityBtn();
         curWeather(storeCity[0]);
     }
 
 };
-
+window.location.href = '/index2.html'
+});
 //displays saved recent searches as button in Recent Searches on page1
 function displayCityBtn() {
-    $("#box").empty();
+    $("#Box").empty();
     for (var i = 0; i < storeCity.length; i++){
         var newBtn = $("<button>");
         newBtn.attr("type", "button");
@@ -105,9 +76,9 @@ function displayCityBtn() {
         newBtn.attr("data-cityName", storeCity[i]);
         newBtn.text(storeCity[i])
 
-        $("#box").append(newBtn);
+        $("#Box").append(newBtn);
     }
-    localStorage.setItem("Cities", JSON.stringify(storeCity));
+    
 }; 
 
 
@@ -177,4 +148,4 @@ function initMap() {
 
     });
   
-
+  
